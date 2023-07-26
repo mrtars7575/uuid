2e31b1e7b449debb3af988ad16efe3e2c39ed37c
murat <mrtars2000@gmail.com>
2023-07-26 11:13:55 +0300

    test

--- app/src/main/java/com/example/uuid/MainActivity.java
+++ app/src/main/java/com/example/uuid/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,80 +20,51 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView tv = findViewById(R.id.tv);
-        TextView tv2 = findViewById(R.id.textView2);
-
-        String androidId = getUniqueId(getApplicationContext());
-
-        tv.setText("android id : " + androidId );
-
-        String uuid = getUniqueId();
-        tv2.setText("uuid with sha256 :" + uuid);
 
 
+        String androidId = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
 
-    }
-    public static String getUniqueId(Context context) {
-        String androidId = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
 
-        // Android ID'nin son 10 hanesini alın
-        String lastTenDigits = androidId.substring(Math.max(0, androidId.length() - 10));
+       /* String androidId = "cd8d6753069e9ef2";*/
+        ArrayList<Integer> intList = new ArrayList<>();
+        ArrayList<Integer> intList2 = new ArrayList<>();
 
-        // Android ID'nin son 10 hanesini yalnızca numerik karakterlere dönüştürün
-        String numericId = toNumeric(lastTenDigits);
+        StringBuilder resultBuilder = new StringBuilder();
+        StringBuilder uniqueID = new StringBuilder();
 
-        // En az 8, en fazla 10 haneli benzersiz kimliği döndürün
-        return numericId.substring(0, Math.min(numericId.length(), 10));
-    }
+        for (int i = 0; i < 16; i += 1) {
+            String hexPair = androidId.substring(i, i + 1);
+            System.out.println("hexPair :" + hexPair );
+            int decimalValue = Integer.parseInt(hexPair, 16);
+            System.out.println("decimalValue : " + decimalValue);
+            intList.add(decimalValue);
+            resultBuilder.append(decimalValue);
 
-    // Heksadesimal formattaki stringi yalnızca numerik forma dönüştürme metodu
-    public static String toNumeric(String hexString) {
-        StringBuilder numericBuilder = new StringBuilder();
+        }
+        int sum;
+        for(int n = 0 ; n < intList.size() - 1  ; n += 2 ){
 
-        for (int i = 0; i < hexString.length(); i++) {
-            char c = hexString.charAt(i);
-            if (Character.isDigit(c)) {
-                numericBuilder.append(c);
+            sum = intList.get(n) + intList.get(n+1);
+            System.out.println("sum : " + sum);
+            if(sum>=10){
+                intList2.add((sum % 10));
+            }else{
+                intList2.add(sum);
             }
         }
 
-        return numericBuilder.toString();
-    }
-
-
-    // SHA 256 metotları
-
-    public static String getUniqueId() {
-        String deviceInfo = getDeviceInfo(); // Cihaz özelliklerini alın
-        String uniqueId = sha256(deviceInfo); // SHA-256 kullanarak hash oluşturun
-        return bytesToNumeric(uniqueId.getBytes()).substring(0, 10); // İlk 10 karakteri alarak 10 haneli numerik kimlik elde edin
-    }
-
-    // Cihazın özelliklerini birleştirme metodu
-    private static String getDeviceInfo() {
-        // Cihazın modeli, seri numarası, üretici bilgisi gibi özellikleri birleştirin
-        return Build.MODEL + Build.SERIAL + Build.MANUFACTURER;
-    }
-
-    // SHA-256 ile hash oluşturma metodu
-    private static String sha256(String input) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            byte[] digest = md.digest(input.getBytes());
-            return bytesToNumeric(digest);
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
+        for (int a : intList2){
+            System.out.println("intList2 : " + a);
         }
-        return null;
+
+        for (int i = 0; i < intList2.size(); i++) {
+            uniqueID.append(intList2.get(i));
+        }
+
+
+        tv.setText(uniqueID);
+
     }
 
-    // Byte dizisini sadece numerik formata dönüştürme metodu
-    private static String bytesToNumeric(byte[] bytes) {
-        StringBuilder numericString = new StringBuilder();
-        for (byte aByte : bytes) {
-            int numericValue = 0xFF & aByte;
-            numericString.append(numericValue);
-        }
-        return numericString.toString();
-    }
 
 }
